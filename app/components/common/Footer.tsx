201afd7dc76924c4dfb3dd9b974e1d7101958dc3
garden <garden4019@gmail.com>
2025-06-16 23:09:22 +0900

    fix: Footer 컴포넌트의 아이콘 타입 오류 수정

--- app/components/common/Footer.tsx
+++ app/components/common/Footer.tsx
@@ -1,10 +1,17 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import { motion } from 'framer-motion'
+import { IconType } from 'react-icons'
 
 interface IconProps {
   className?: string;
   'aria-hidden'?: boolean;
+}
+
+interface SocialLink {
+  name: string
+  href: string
+  icon: IconType
 }
 
 const navigation = {
@@ -21,8 +28,8 @@ const navigation = {
     {
       name: 'Instagram',
       href: '#',
-      icon: (props: IconProps) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+      icon: ({ className, 'aria-hidden': ariaHidden }: { className?: string, 'aria-hidden'?: boolean }) => (
+        <svg className={className} fill="currentColor" viewBox="0 0 24 24" aria-hidden={ariaHidden}>
           <path
             fillRule="evenodd"
             d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
@@ -34,8 +41,8 @@ const navigation = {
     {
       name: 'YouTube',
       href: '#',
-      icon: (props: IconProps) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+      icon: ({ className, 'aria-hidden': ariaHidden }: { className?: string, 'aria-hidden'?: boolean }) => (
+        <svg className={className} fill="currentColor" viewBox="0 0 24 24" aria-hidden={ariaHidden}>
           <path
             fillRule="evenodd"
             d="M19.812 5.418c.861.23 1.538.907 1.768 1.768C21.998 8.746 22 12 22 12s0 3.255-.418 4.814a2.504 2.504 0 0 1-1.768 1.768c-1.56.419-7.814.419-7.814.419s-6.255 0-7.814-.419a2.505 2.505 0 0 1-1.768-1.768C2 15.255 2 12 2 12s0-3.255.417-4.814a2.507 2.507 0 0 1 1.768-1.768C5.744 5 11.998 5 11.998 5s6.255 0 7.814.418ZM15.194 12 10 15V9l5.194 3Z"
@@ -47,11 +54,11 @@ const navigation = {
     {
       name: 'Kakao',
       href: '#',
-      icon: (props: IconProps) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+      icon: ({ className, 'aria-hidden': ariaHidden }: { className?: string, 'aria-hidden'?: boolean }) => (
+        <svg className={className} fill="currentColor" viewBox="0 0 24 24" aria-hidden={ariaHidden}>
           <path
             fillRule="evenodd"
-            d="M12 3C6.477 3 2 6.477 2 12c0 5.523 4.477 10 10 10s10-4.477 10-10c0-5.523-4.477-10-10-10zm0 18.75c-4.832 0-8.75-3.918-8.75-8.75S7.168 4.25 12 4.25s8.75 3.918 8.75 8.75-3.918 8.75-8.75 8.75zm0-15.5c-3.728 0-6.75 2.382-6.75 5.321 0 1.887 1.262 3.541 3.164 4.465l-.808 2.983a.25.25 0 00.385.275l3.474-2.325c.174.011.348.017.523.017 3.728 0 6.75-2.382 6.75-5.321 0-2.939-3.022-5.321-6.75-5.321z"
+            d="M12 3C7.03 3 3 6.37 3 10.5c0 2.89 1.92 5.41 4.76 6.67-.16.57-.6 2.06-.69 2.38-.11.4.15.39.31.28.13-.08 2.04-1.39 2.87-1.95.57.08 1.16.12 1.75.12 4.97 0 9-3.37 9-7.5S16.97 3 12 3"
             clipRule="evenodd"
           />
         </svg>
@@ -136,7 +143,7 @@ export default function Footer() {
                   className="text-white/80 hover:text-white transition-colors"
                 >
                   <span className="sr-only">{item.name}</span>
-                  <item.icon className="h-6 w-6" aria-hidden="true" />
+                  <item.icon className="h-6 w-6" aria-hidden={true} />
                 </Link>
               ))}
             </div>
