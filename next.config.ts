c5163bf517ea54e0509e44e82fb4eee4923609f0
garden <garden4019@gmail.com>
2025-06-18 03:30:12 +0900

    fix: CSS와 JavaScript 설정 수정

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,22 @@
+/** @type {import('next').NextConfig} */
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: ['images.unsplash.com'],
   },
   experimental: {
     optimizeCss: true,
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader'],
+    });
+    return config;
   },
 };
 
