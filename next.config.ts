7a33bdc016ae04226ba4258aa271220a67686d23
garden <garden4019@gmail.com>
2025-06-18 03:32:59 +0900

    fix: Next.js 설정 수정 및 globals.css 배경 스타일 수정

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,12 @@
 /** @type {import('next').NextConfig} */
 
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ['images.unsplash.com'],
   },
   experimental: {
     optimizeCss: true,
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader', 'postcss-loader'],
-    });
-    return config;
   },
 };
 
